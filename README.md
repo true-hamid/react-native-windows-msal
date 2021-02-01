65ff0591330497a2dad5322e333873ac438ab4f5
hamid <hehamid97@gmail.com>
2021-02-02 02:17:10 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-windows-msal
+ A simple module exposing MSAL login from Windows native to react native
---
+++ windows/RNWMsal/ReactNativeWindowsMsal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ReactNative.Managed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNWMsal
+{
+    [ReactModule]
+    class ReactNativeWindowsMsal
+    {
+        [ReactConstant]
+        public double E = Math.E;
+
+        [ReactConstant("Pi")]
+        public double PI = Math.PI;
+
+        [ReactMethod("add")]
+        public double Add(double a, double b)
+        {
+            double result = a + b;
+            return result;
+        }
+    }
+}
