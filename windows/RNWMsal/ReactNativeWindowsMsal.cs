96616ceef93bb6e2d0416d46941aaa2a19a4bfac
hamid <hehamid97@gmail.com>
2021-02-15 20:29:43 +0400

    removed unnecessary methods/ renamed class

--- windows/RNWMsal/ReactNativeWindowsMsal.cs
+++ windows/RNWMsal/ReactNativeWindowsMsal.cs
@@ -7,32 +7,10 @@ namespace RNWMsal
     [ReactModule]
     class ReactNativeWindowsMsal
     {
-        [ReactConstant]
-        public double E = Math.E;
-
-        [ReactConstant("Pi")]
-        public double PI = Math.PI;
-
-        [ReactConstant]
-        public string STR = "TEST NATIVE MODULE";
-
-        [ReactMethod]
-        public double subtract(double a, double b)
-        {
-            double result = a - b;
-            return result;
-        }
-
-        [ReactMethod("add")]
-        public void Add(double a, double b)
-        {
-            double result = a + b;
-        }
-
         [ReactMethod("getLoginToken")]
         public async void GetLoginTokenAsync(JSValue parameters, IReactPromise<string> promise)
         {
-            string result = await MsalLogin.CallLoginAPI(parameters);
+            string result = await MsalAuthentication.CallLoginAPI(parameters);
             promise.Resolve(result);
         }
     }
